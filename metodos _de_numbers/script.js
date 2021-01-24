98a19beeab660198143dd72f5d517367e1f1040f
José Roberto <tekinforroberto@gmail.com>
2021-01-24 17:45:25 -0300

    Manipulando Numeros

---
+++ metodos _de_numbers/script.js
@@ -0,0 +1,11 @@
+let numero = '25';
+
+
+// Formata um número utilizando notação de ponto fixo.
+// let resultado = 'R$ ' + numero.toFixed(2);
+
+// Retorna um inteiro na base especificada.
+// let resultado = parseInt(numero);
+
+console.log("Resultado: ", resultado);
+

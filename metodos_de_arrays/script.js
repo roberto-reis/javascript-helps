6143fbf9659cc55f0b93ab0c0b2b64389befcd3b
José Roberto <tekinforroberto@gmail.com>
2021-01-24 21:53:18 -0300

    Manipulando Arrays

---
+++ metodos_de_arrays/script.js
@@ -0,0 +1,60 @@
+let lista = ['Ovo', 'Farinha', 'Corante', 'Massa'];
+let lista2 = [45, 4, 9, 16, 25];
+let lista3 = [];
+
+// junta todos os elementos de um array (ou um array-like object) em uma string e retorna esta string.
+// let res = lista.join();
+
+// remove o primeiro elemento de um array e retorna esse elemento.
+// let res = lista.shift();
+
+//remove o último elemento de um array e retorna aquele elemento.
+// let res = lista.pop();
+
+// adiciona um ou mais elementos ao final de um array e retorna o novo comprimento desse array.
+// let res = lista.push('Leite');
+
+// altera o conteúdo de uma lista, adicionando novos elementos enquanto remove elementos antigos.
+// let res = lista.splice(1, 1);
+
+// retorna um novo array contendo todos os arrays ou valores passados como parâmetro
+// let res = lista.concat(lista2);
+
+// ordena os elementos do próprio array e retorna o array
+// lista.sort();
+
+// inverte os itens de um array.
+// lista.reverse();
+
+// invoca a função callback passada por argumento para cada elemento do Array e devolve um novo Array como resultado
+/* lista3 = lista2.map(function(item) {
+    return item * 2;
+}); */
+
+// cria um novo array com todos os elementos que passaram no teste implementado pela função fornecida.
+/* lista3 = lista2.filter(function(item) {
+    if(item < 20) {
+        return true;
+    } else {
+        return false;
+    }
+}); */
+
+// testa se todos os elementos do array passam pelo teste implementado pela função fornecida.
+/* lista3 = lista2.every(function(item) {
+    if(item > 20) {
+        return true;
+    } else {
+        return false;
+    }
+}); */
+
+// testa se ao menos um dos elementos no array passa no teste implementado pela função atribuída e retorna um valor true ou false.
+/* lista3 = lista2.some(function(item) {
+    return (item > 3) ? true : false;
+}); */
+
+let res = lista3;
+
+console.log("Resultado: ", res);
+

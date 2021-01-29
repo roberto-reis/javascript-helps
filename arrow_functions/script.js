c75a822d95b2dd42ecbada0e68f58bbae9aa9507
José Roberto <tekinforroberto@gmail.com>
2021-01-28 22:17:41 -0300

    Trabalhando com Arrow Functions

---
+++ arrow_functions/script.js
@@ -0,0 +1,16 @@
+/* function somar(x, y) {
+    return x + y;
+}
+ */
+
+/* let somar = function(x, y) {
+    return x + y;
+} */
+
+/* let somar = (x, y) => {
+    return x + y;
+} */
+
+let somar = (x, y) => x + y;
+
+console.log( somar(8, 8) );

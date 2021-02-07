8f95ca57d7e49c1c81cd9ace45bbab7e53ec140d
José Roberto <tekinforroberto@gmail.com>
2021-02-06 21:53:32 -0300

    Criando uma calculadora com Gulp

---
+++ calculadora-gulp/src/js/home.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    document.getElementById("calcular").addEventListener('click', function() {
+        let n1 = parseFloat(document.getElementById("n1").value);
+        let n2 = parseFloat(document.getElementById("n2").value);
+
+        if(n1 >= 0 && n2 >= 0) {
+            let res = n1 + n2;
+            document.getElementById("resultado").innerHTML = "Soma é: " + res;
+        } else {
+            document.getElementById("resultado").innerHTML = "Preencha os campos!";
+        }
+
+        
+
+        
+    });
+}
